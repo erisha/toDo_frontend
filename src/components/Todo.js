64e10b56f2002b42dbe8974fa4372a476d0772a9
Erisha Roach <forever.erisha@gmail.com>
2024-02-11 17:27:41 -0500

    added complete task function

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from 'react';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
-
-export const Todo = ({task}) => {
+export const Todo = ({ task, toggleComplete }) => {
   return (
-    <div className='Todo'>
-      <p>{task.task}</p>
+    <div className="Todo">
+      <p 
+        className={`$task.completed ? 'completed' : "" `} onClick={() => toggleComplete(task.id)}>
+        {task.task}
+      </p>
       <div>
         <EditIcon />
         <DeleteIcon />
       </div>
     </div>
-  )
-}
+  );
+};
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -8,21 +8,33 @@ uuidv4();
 
 export const TodoContainer = () => {
   const [todos, setTodos] = useState([]);
+  // add todo
   const addTodo = (todo) => {
     setTodos([
       ...todos,
       { id: uuidv4(), task: todo, completes: false, isEditing: false },
     ]);
-
     console.log(todos);
   };
+
+// mark task complete
+  const toggleComplete = id => { 
+    setTodos(todos.map(todo => todo.id === id ? {
+      todo, completed: !todo.completed} : todo
+      ))}
   return (
     <div className="TodoContainer">
+      <h1>My ToDos</h1>
       <TodoForm addTodo={addTodo} />
+
+      {/* list task */}
       {todos.map((todo, index) => (
         <Todo 
           task={todo} 
-          key={index}/>
+          key={index}
+          toggleComplete={toggleComplete}
+          
+          />
       ))}
 
   
