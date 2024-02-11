a74128076bfec0b5d292f4cd425bced1c7c5bca3
Erisha Roach <forever.erisha@gmail.com>
2024-02-11 12:36:43 -0500

    added useState to to todoForm to make it functional & save input

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const TodoForm = () => {
+    const [value, setValue] = useState("")
+    
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(value);
+    }
   return (
-    <form className='TodoForm'>
-        <input type='text' className='todo-input' placeholder='New Task'/>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+        <input type='text' 
+        className='todo-input' 
+        placeholder='New Task'
+        onChange={(e) => setValue(e.target.value)}//saves input to state
+        />
+        
         <button type='submit' className='todo-btn'>Add Task</button>
         </form>
   )
